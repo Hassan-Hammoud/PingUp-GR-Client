e10893b46422a30483bbbb80483e242f41e10acd
Hassan Hammoud <hassanhammoud2000@gmail.com>
2025-11-06 16:45:06 +0200

    StoriesBar component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
 export { default as Loading } from './Loading';
 export { default as MenuItems } from './MenuItems';
 export { default as Sidebar } from './Sidebar';
+export { default as StoriesBar } from './StoriesBar';
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from 'react';
+import { dummyPostsData } from '../assets/assets';
+import { Loading, StoriesBar } from '../components';
+
 const Feed = () => {
-  return <div>Feed</div>;
+  const [feeds, setFeeds] = useState([]);
+  console.log('🚀 ~ Feed ~ feeds:', feeds);
+  const [loading, setLoading] = useState(true);
+
+  // FETCH FEEDS
+  const fetchFeeds = async () => {
+    setFeeds(dummyPostsData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+      {/* STORIES AND POST LIST  */}
+      <div className=''>
+        <StoriesBar />
+        <div className='p-4 space-y-6'>List Of Post</div>
+      </div>
+      {/* RIGHT SIDE BAR  */}
+      <div className=''>
+        <div className=''>
+          <h1 className=''>Sponsored</h1>
+        </div>
+        <div className=''>
+          <h1 className=''>Recent Messages</h1>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
 };
 
 export default Feed;
