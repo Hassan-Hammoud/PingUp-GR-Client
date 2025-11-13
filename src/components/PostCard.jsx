34dd79de2e78a46d9afb8b907a4c4813933fd34e
Hassan Hammoud <hassanhammoud2000@gmail.com>
2025-11-13 13:32:33 +0200

    Messages components

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,6 +1,7 @@
 import { BadgeCheck, Heart, MessageCircle, Share2 } from 'lucide-react';
 import moment from 'moment';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { dummyUserData } from '../assets/assets';
 const PostCard = ({ post }) => {
   const postWithHashtags = post.content.replace(
@@ -15,10 +16,14 @@ const PostCard = ({ post }) => {
     await setLikes(prevLikes => prevLikes.includes(currentUser._id));
   };
 
+  const navigate = useNavigate();
   return (
     <div className='bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl:'>
       {/* USER INFO */}
-      <div className='inline-flex items-center gap-3 cursor-pointer'>
+      <div
+        onClick={() => navigate('/profile/' + post.user._id)}
+        className='inline-flex items-center gap-3 cursor-pointer'
+      >
         <img
           src={post.user.profile_picture}
           className='w-10 h-10 rounded-full'
