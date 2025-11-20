7de02767a74e9febd7c1fb354e18a8f8580086ee
Hassan Hammoud <hassanhammoud2000@gmail.com>
2025-11-20 15:21:34 +0200

    like and unlike post

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,22 +3,46 @@ import moment from 'moment';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // import { dummyUserData } from '../assets/assets';
+import { useAuth } from '@clerk/clerk-react';
+import { toast } from 'react-hot-toast';
 import { useSelector } from 'react-redux';
+import api from './../api/axios.js';
 const PostCard = ({ post }) => {
   const postWithHashtags = post.content.replace(
     /(#\w+)/g,
     '<span class="text-indigo-600">$1</span>'
   );
+  const { getToken } = useAuth();
 
-  const [likes, setLikes] = useState([post.likes_count]); //! check this array
+  const [likes, setLikes] = useState(post.likes_count);
 
   const currentUser = useSelector(state => state.user.value);
-  console.log('🚀 ~ PostCard ~ currentUser:', currentUser._id);
 
   const handleLike = async () => {
-    await setLikes(prevLikes => {
-      return prevLikes.includes(currentUser._id);
-    });
+    try {
+      const { data } = await api.post(
+        `/api/post/like`,
+        { postId: post._id },
+        {
+          headers: { Authorization: `Bearer ${await getToken()}` },
+        }
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        setLikes(prev => {
+          if (prev.includes(currentUser._id)) {
+            return prev.filter(id => id !== currentUser._id);
+          } else {
+            return [...prev, currentUser._id];
+          }
+        });
+      } else {
+        toast(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   const navigate = useNavigate();
@@ -69,10 +93,20 @@ const PostCard = ({ post }) => {
       {/* ACTIONS BUTTONS */}
       <div className='flex items-center gap-4 text-gray-600 text-sm pt-2 border-t border-gray-300'>
         <div className='flex items-center gap-1'>
-          <Heart
+          {/* <Heart
             onClick={handleLike}
             className={`w-4 h-4 cursor-pointer${
               likes.includes(currentUser._id) && 'text-red-500 fill-red-500'
+            }`}
+          /> */}
+          <Heart
+            onClick={handleLike}
+            className={`w-4 h-4 cursor-pointer ${
+              Array.isArray(likes) &&
+              currentUser?._id &&
+              likes.includes(currentUser._id)
+                ? 'text-red-500 fill-red-500'
+                : ''
             }`}
           />
           <span className=''>{likes.length}</span>
